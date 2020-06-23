1c9c25123f661a70a53ed81f6b5873ceb15249a3
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-23 21:13:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,6 @@
 class Shoe
+  BRANDS = []
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
@@ -10,5 +12,6 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  
+  
 end
